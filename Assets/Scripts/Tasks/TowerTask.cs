26399a12df2da38191ca92d54ab32790151c8014
Sebastian Lague <sebastlague@gmail.com>
2025-07-31 13:50:06 +0200

    hanoi working

--- Assets/Scripts/Tasks/TowerTask.cs
+++ Assets/Scripts/Tasks/TowerTask.cs
@@ -7,16 +7,22 @@ public class TowerTask : Task
 	public BoxCollider[] poles;
 	public BoxCollider[] disks;
 
-	public List<int> poleStart = new();
-	public List<int> poleEnd = new();
-	public List<int> poleMiddle = new();
+	List<int> poleStart = new();
+	List<int> poleEnd = new();
+	List<int> poleMiddle = new();
+
+	float startY;
+	float spacingY;
 
 	Color[] diskCols;
+	int selectedDiskIndex;
 
 	void Start()
 	{
 		poleStart.AddRange(new int[] { 0, 1, 2, 3 });
 		diskCols = disks.Select(d => d.gameObject.GetComponent<MeshRenderer>().material.color).ToArray();
+		startY = disks[0].transform.localPosition.z;
+		spacingY = disks[1].transform.localPosition.z - startY;
 
 		// ----- Test state
 		if (!taskActive) ExitTask();
@@ -34,11 +40,13 @@ public class TowerTask : Task
 
 		if (Input.GetMouseButtonDown(0))
 		{
-			int selectedDiskIndex = GetNearestMouseOverIndex(disks);
+			int newSelectedDiskIndex = GetNearestMouseOverIndex(disks);
+			int selectedPoleIndex = GetNearestMouseOverIndex(poles);
 
-			if (selectedDiskIndex != -1)
+			if (newSelectedDiskIndex != -1)
 			{
-				Debug.Log(selectedDiskIndex);
+				selectedDiskIndex = newSelectedDiskIndex;
+
 				for (int i = 0; i < disks.Length; i++)
 				{
 					Color c = diskCols[i];
@@ -48,20 +56,95 @@ public class TowerTask : Task
 
 				disks[selectedDiskIndex].gameObject.GetComponent<MeshRenderer>().sharedMaterial.color = diskCols[selectedDiskIndex];
 			}
-			else
+			else if (selectedDiskIndex != -1 && selectedPoleIndex != -1)
 			{
-				int selectedPoleIndex = GetNearestMouseOverIndex(poles);
+				int currMaxOnPole = GetPoleValue(selectedPoleIndex);
+				//Debug.Log($"Selected pole: {selectedPoleIndex}  with topDiskIndex = {currMaxOnPole}");
+
+				// can move to pole
+				if (selectedDiskIndex > currMaxOnPole)
+				{
+					//Debug.Log($"Can move to pole: {selectedDiskIndex} > {currMaxOnPole}");
+					int currPoleIndex = GetPoleIndexFromDiskIndex(selectedDiskIndex);
+					// is at top of curr pole (can move from this pole)
+					if (GetPoleValue(currPoleIndex) == selectedDiskIndex)
+					{
+						disks[selectedDiskIndex].transform.localPosition = new Vector3(0, poles[selectedPoleIndex].transform.localPosition.y, startY + GetPoleFromPoleIndex(selectedPoleIndex).Count * spacingY);
+						PopFromPole(currPoleIndex);
+						AddToPole(selectedPoleIndex, selectedDiskIndex);
+						selectedDiskIndex = -1;
+						ResetCols();
+						//Debug.Log($"Can move from pole (is at top): {GetPoleValue(currPoleIndex)} == {selectedDiskIndex}  pos = {poles[selectedPoleIndex].transform.localPosition.y}");
+					}
+				}
 			}
 		}
+
+		// Task completion
+		if (poleEnd.Count == 4)
+		{
+			Debug.Log("Tower completed");
+			taskCompleted = true;
+		}
+
+		// -------- Exit
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			ExitTask();
+		}
+	}
+
+	List<int> GetPoleFromPoleIndex(int p)
+	{
+		if (p == 0) return poleStart;
+		if (p == 1) return poleMiddle;
+		return poleEnd;
+	}
+
+	int GetPoleIndexFromDiskIndex(int diskIndex)
+	{
+		if (poleStart.Contains(diskIndex)) return 0;
+		if (poleMiddle.Contains(diskIndex)) return 1;
+		return 2;
+	}
+
+	int GetPoleValue(int poleIndex)
+	{
+		List<int> pole = GetPoleFromPoleIndex(poleIndex);
+		if (pole.Count == 0) return -1;
+
+		return pole[^1];
+	}
+
+	void PopFromPole(int poleIndex)
+	{
+		List<int> pole = GetPoleFromPoleIndex(poleIndex);
+		if (pole.Count > 0) pole.RemoveAt(pole.Count - 1);
+	}
+
+	void AddToPole(int poleIndex, int value)
+	{
+		List<int> pole = GetPoleFromPoleIndex(poleIndex);
+		pole.Add(value);
 	}
 
 	public override void EnterTask(FirstPersonController controller)
 	{
 		base.EnterTask(controller);
+		selectedDiskIndex = -1;
 	}
 
 	public override void ExitTask()
 	{
 		base.ExitTask();
+		ResetCols();
+	}
+
+	void ResetCols()
+	{
+		for (int i = 0; i < disks.Length; i++)
+		{
+			disks[i].gameObject.GetComponent<MeshRenderer>().sharedMaterial.color = diskCols[i];
+		}
 	}
 }

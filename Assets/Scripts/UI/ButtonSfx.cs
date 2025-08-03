6abc874e943453c29c7dd1adbbf21b965e5ee09f
Sebastian Lague <sebastlague@gmail.com>
2025-08-03 14:40:39 +0200

    music

--- Assets/Scripts/UI/ButtonSfx.cs
+++ Assets/Scripts/UI/ButtonSfx.cs
@@ -8,8 +8,8 @@ public class ButtonSfx : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
 	
 	public void OnPointerEnter(PointerEventData eventData)
 	{
-		GameManager.Instance.audioSource2D.pitch = 1 + Mathf.Sin(Time.time * 6) * 0.1f;
-		if (mouseOverSfx.clip) GameManager.Instance.audioSource2D.PlayOneShot(mouseOverSfx.clip, mouseOverSfx.volumeT * Random.Range(0.8f, 1));
+		GameManager.Instance.audioSource2D.pitch = 1 + Mathf.Sin(Time.time * 5) * 0.1f;
+		if (mouseOverSfx.clip) GameManager.Instance.audioSource2D.PlayOneShot(mouseOverSfx.clip, mouseOverSfx.volumeT * Random.Range(0.7f, 0.9f));
 	}
 
 	public void OnPointerExit(PointerEventData eventData)
